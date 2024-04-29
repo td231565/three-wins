be3bb853db6758c296c3a39290e56eee1e6b8143
Richard Chen <td231565@gmail.com>
2024-04-29 16:51:47 +0800

    fix(deploy) descriptions: - modify nuxt.config branch main task

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,17 @@ export default defineNuxtConfig({
     '@fontsource/inter/600.css'
   ],
 
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+     cssnano:
+       process.env.NODE_ENV === 'production'
+         ? { preset: ['default', { discardComments: { removeAll: true } }] }
+         : false, // disable cssnano when not in production
+    },
+  },
+
   modules: [
     '@nuxtjs/prismic',
     '@nuxtjs/tailwindcss'
